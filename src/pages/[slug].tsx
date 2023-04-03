a67e4ded48d5fff9dd4261c39b7c2ca9154d6a94
JoeLisk <josephlisk13@gmail.com>
2023-04-02 19:27:52 -0500

    render posts on profile page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -2,6 +2,20 @@ import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { api } from "~/utils/api";
 
+const ProfileFeed = (props: {userId: string}) => {
+
+  const {data, isLoading} = api.posts.getPostsByUserId.useQuery({userId: props.userId});
+
+  if (isLoading) return <LoadingPage />;
+
+  if (!data || data.length === 0) return <div>User has not posted</div>
+
+  return (
+    <div className="flex flex-col">
+      {data.map(fullPost => (<PostView {...fullPost} key={fullPost.post.id} />))}
+    </div>
+  );
+}
 
 const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
 
@@ -31,6 +45,7 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
         <div className="h-[64px]"></div>
         <div className="p-4 text-2xl font-bold">{`@${data.username ?? ""}`}</div>
         <div className="border-b border-slate-400 w-full"></div>
+      <ProfileFeed userId={data.id} />
       </PageLayout>
     </>
   );
@@ -42,6 +57,8 @@ import { prisma } from "~/server/db";
 import superjson from "superjson";
 import { PageLayout } from "~/components/layout";
 import Image from "next/image";
+import { LoadingPage } from "~/components/loading";
+import { PostView } from "~/components/postview";
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const ssg = createProxySSGHelpers({
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,12 @@
 import { type NextPage } from "next";
-import Head from "next/head";
-
 import { SignInButton, useUser } from "@clerk/nextjs";
-
 import { api } from "~/utils/api";
-import type { RouterOutputs } from "~/utils/api";
-
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
-import Link from "next/link";
 import { PageLayout } from "~/components/layout";
-
-dayjs.extend(relativeTime);
+import { PostView } from "~/components/postview";
 
 const CreatePostWizard = () => { 
   const { user } = useUser();
@@ -75,34 +66,6 @@ const CreatePostWizard = () => {
       <LoadingSpinner size={20} />
     </div> )}
   </div>
-}
-
-type PostWithUser = RouterOutputs["posts"]["getAll"][number]; //[number] means we want an element of the array
-
-const PostView = (props: PostWithUser ) => {
-  const { post, author } = props;
-  return (
-    <div key={post.id} className="flex gap-3 p-4 border-b border-slate-400">
-      <Image 
-        src={author.profileImageUrl} 
-        alt={`@${author.username}'s profile picture`} 
-        className="w-14 h-14 rounded-full" 
-        width={56}
-        height={56}
-      />
-      <div className="flex flex-col">
-        <div className="flex text-slate-300 gap-1">
-          <Link href={`/@${author.username}`}>
-            <span>{`@${author.username}`}</span>
-          </Link>
-          <Link href={`/post/${post.id}`}>
-            <span className="font-thin">{`  · ${dayjs(post.createdAt).fromNow()}`}</span>
-          </Link>
-        </div>
-        <span className="text-xl">{post.content}</span>
-      </div>
-    </div>
-  )
 }
 
 const Feed = () => {
