091e2a4710a6ac91fe87c0ade71902a22eab4401
JoeLisk <josephlisk13@gmail.com>
2023-04-02 20:03:05 -0500

    separate ssg helper, full post page

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,22 +1,51 @@
-import { type NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import { api } from "~/utils/api";
+import { PageLayout } from "~/components/layout";
+import { PostView } from "~/components/postview";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 
-//[id].tsx - when we use Nextjs router, will have access to the id
+const SinglePostPage: NextPage<{ id: string }> = ({ id }) => {
 
-const SinglePostPage: NextPage = () => {
+  const {data} = api.posts.getById.useQuery({
+    id,
+  });
+
+  if (!data) return <div>404</div>;
+
+  // important reason to not user getServersideProps at 2:06:00
 
   return (
     <>
       <Head>
-        <title>Post</title>
+        <title>{`${data.post.content} - @${data.author.username}`}</title>
       </Head>
-      <main className="flex justify-center h-screen">
-      <div>
-       Single Post Page
-      </div>
-      </main>
+      <PageLayout>
+        <PostView {...data} />
+      </PageLayout>
     </>
   );
 };
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = generateSSGHelper();
+
+  const id = context.params?.id;
+
+  if (typeof id !== "string") throw new Error("no slug");
+
+  await ssg.posts.getById.prefetch({ id });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      id,
+    }
+  }
+}
+
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" }
+}
 
 export default SinglePostPage;
